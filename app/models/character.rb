d093a7f35c1119ea5fb500a14d45985dfb9fb11b
yukijina <yukijina@gmail.com>
2019-02-22 20:38:40 -0800

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,16 +1,9 @@
 class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
-  
-  def say_that_thing_you_say 
+
+  def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
-  end 
-  
-  def building
-  binding.pry
-    self.build_show(name: self.name)
-    self.build_network(name: self.name)
-   end 
- 
-  
+  end
+
 end
---
+++ db/migrate/008_add_shows_genre.rb
@@ -0,0 +1,5 @@
+class AddShowsGenre < ActiveRecord::Migration[5.2]
+  def change
+    add_column :shows, :genre, :string
+  end
+end
