e38973e60e25fcc8cfadaced2fa7d1d734fdffb2
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-24 16:26:19 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,8 @@
 class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
+  
+  # belongs_to show provieds a build_show method on a character object
 
   def say_that_thing_you_say 
     "#{self.name} always says: #{self.catchphrase}"
