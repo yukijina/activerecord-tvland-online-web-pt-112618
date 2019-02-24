a16062a0535c935fbfb632dd44612e713d18178f
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-24 16:09:30 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,13 +1,6 @@
 class Show < ActiveRecord::Base
-<<<<<<< HEAD
   has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
 
 end
-=======
-  has_many :characters 
-  has_many :actors, through: :characters
-  
-end
->>>>>>> 412d2aa71fe71568dafea562cda3a83c2369ae96
